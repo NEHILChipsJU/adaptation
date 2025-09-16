f9be700e49932d7ef94752feabe11dc802bc6e70
GemmaInfantes <gemmainfantesllinares@gmail.com>
2025-09-16 14:39:08 +0200

    introducing qualitative evaluation for the degradation

---
+++ network_degradation_loop.py
@@ -0,0 +1,254 @@
+#Imports
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import argparse
+
+from utils.utils import T_instant
+from utils.rnn_utils import rnn_params
+from utils.rnn_utils import forward_rnn
+from utils.rnn_utils import forward_rnn_deg
+from utils.rnn_utils import forward_rnn_mix_deg
+from utils.rnn_utils import forward_rnn_mix
+from utils.rnn_utils import forward_rnn_CL_deg
+from utils.rnn_utils import forward_rnn_interp
+from utils.rnn_utils import forward_rnn_CL
+# from utils.rnn_utils import forward_rnn_CL_mix
+from utils.utils import visualize_PCA_3D
+from utils.utils import PCA_3D
+from utils.utils import visualize_multiple_PCA_3D
+from utils.rnn_utils import ridge
+from utils.rnn_utils import compute_conceptor
+from utils.utils import visualize_sine_interpolation
+from utils.utils import visualize_sine_interpolation_one
+# from utils.rnn_utils import degradation
+from utils.rnn_utils import trained_model_new
+
+#Parameters that we want to acces trough the terminal
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--m", type=int, default=2)
+parser.add_argument("--random", type=int, default=5)
+parser.add_argument("--threshold", type=float, default=0.2)
+args = parser.parse_args()
+
+
+#########################################################################
+
+##Random inicialization of the ESN, with the parameters of the paper
+
+########################################################################
+
+spectral_radius=1.6
+scaling=1
+bias_scaling=1
+alpha=0.75 #Leakage
+a=25 #Aperture.  
+N=256 # Network size 256
+nu=2.5e-5 #Learning Rate
+beta=0.2 #Control gain
+washout=20 # steps we wait until the network is stable, in order to show the results
+reg=1 #regularization parameter in the Ride regression
+step=1 # number of steps that the model will predict
+sparsity=None
+# deg=90
+
+
+#Input Signal
+def sin(T,t, A):
+    """Sinus wave with a given period T, and a time series"""
+    s=A*np.sin((2*np.pi*t)/T)
+    return s
+
+#Temporal Series
+points=40 #points in one periode
+A=1 #Sinus Amplitude
+# now we will use more than one input 
+T1=20 #fixed period
+dt=T1/points
+t=np.arange(0, 300,dt)
+# plt.plot(sin(T1,t,A),'.-')
+# plt.xlim([0,45])
+#obtaining the input and some parameters for params
+data1=sin(T1,t,A)
+data1=data1.reshape(-1, 1)
+#create shifted input and output, the output is the target
+ut_train1 = data1[:-step]               # shape (N-step, 1)
+yt_train1 = data1[step:]                # shape (N-step, 1)
+#get dimensions
+input_size=ut_train1.shape[-1]
+output_size=yt_train1.shape[-1]
+
+#build a dictionary with the parameters that will form the ESN, so we have them organized
+#initalize parameters
+params=rnn_params(
+    N,
+    input_size,
+    output_size,
+    scaling,
+    spectral_radius,
+    alpha,
+    bias_scaling,
+    sparsity,
+    seed=1235
+)
+
+#obtain matrix X1 (time, N) of internal states for all time points
+X1=forward_rnn(params, ut_train1, x_init=None,autonomous=False,conceptor=None)
+#Compute model conceptors
+C1=compute_conceptor(X1, a)
+#getting the final Wout with the ridge regression (Wout=Ytarget*X.T*(X*X.T+beta*I)^-1)
+X_effective = X1[washout:]
+yt_train_effective = yt_train1[washout:]
+#showing training X
+#training Wout with Xi applying Ci
+params_trained, mse = ridge(reg, X_effective, yt_train_effective,step,params) #this gives us the results for the trainning dataset
+
+####################################################################################
+
+#Stabilization against partial network degradation
+
+######################################################################################
+
+#Setting a better C_init
+#define a variable with the training data and the targets (to predict the last point of the time series)
+T2=30
+data2=sin(T2,t,A)
+data2=data2.reshape(-1, 1)
+#create shifted input and output, the output is the target
+ut_train2 = data2[:-step]               # shape (N-step, 1)
+yt_train2 = data2[step:]                # shape (N-step, 1)
+##Extracting X for each T2
+X2=forward_rnn(params, ut_train2, x_init=None,autonomous=False,conceptor=None)
+C2=compute_conceptor(X2, a)
+
+
+
+# C_init=(C1+C2)/2
+C_init=np.eye(N,N)
+X_init=None
+C_t=C1
+#keeping just the degraded part
+last=int(len(ut_train1)/3)
+
+#No degradation
+PCA_nodeg=visualize_PCA_3D(X1[-last:],f"X for NO deg ")
+mse_cte=trained_model_new(X1[washout:],ut_train1,yt_train1,params_trained,washout,True,None)
+
+#computing the results and showing them
+m=args.m #neurons degraded per step
+random=args.random #number of trials
+k=np.arange(0,10,m) #number of degraded nodes
+np.random.seed(42) #if we want this seed to be alwas de same  
+seed=np.random.randint(0, 2000, size=random) #seed for the random degradation
+x_C=[] # storing 0 o 1, for succes or fail
+x_CCL=[] # storing 0 o 1, for succes or fail
+x_mix=[] # storing 0 o 1, for succes or fail
+rate_C=[] # storing failure rate
+rate_CCL=[] # storing failure rate
+rate_mix=[] # storing failure rate
+std_C=[] #standard desviation
+std_CCL=[]
+std_mix=[]
+#Loop removing k nodes in each step
+for i in tqdm(range(len(k))):
+    for j in range(len(seed)):
+        deg= (k[i]/N)*100 # % of degradation
+           
+        #C=cte with degradation
+        X_cte_deg=forward_rnn_deg(params_trained, ut_train1,X_init, True,C1,deg,seed[j])
+        PCA_C,_,_=PCA_3D(X_cte_deg[-last:],f"X for {deg}% deg with C=C1) ")
+        
+        
+        #CCL (separed) with degradation
+        X_CL_deg,_=forward_rnn_CL_deg(nu,a,beta,params_trained, ut_train1,C_init,C_t,None,None,X_init,True,deg,seed[j]) #x% of degradation
+        PCA_CCL,_,_=PCA_3D(X_CL_deg[-last:],f"X for {deg}% deg with CCL (separed) ")
+        
+        
+        
+        #CCL (mixed) with degradation
+        X_CL_deg_mix,_=forward_rnn_mix_deg(nu,a,beta,params_trained, ut_train1,C_init,C_t,None,None,X_init,True,deg,seed[j]) #x% of degradation
+        PCA_mix,_,_=PCA_3D(X_CL_deg_mix[-last:],f"X for {deg}% deg with CCL (mixed) ")
+        
+        if np.var(PCA_C)>args.threshold*np.var(PCA_nodeg):
+            x_C.append(0) #because we want to compute de failure rate
+        else:
+            x_C.append(1)
+        if np.var(PCA_CCL)>args.threshold*np.var(PCA_nodeg):
+            x_CCL.append(0)
+        else:
+            x_CCL.append(1)  
+        if np.var(PCA_mix)>args.threshold*np.var(PCA_nodeg):
+            x_mix.append(0)
+        else:
+            x_mix.append(1) 
+    # prob of x=1
+    p_C = np.mean(x_C)
+    p_CCL = np.mean(x_CCL)
+    p_mix = np.mean(x_mix)
+    
+    # standard desviatimn (Bernoulli)
+    std_C.append( np.sqrt(p_C * (1 - p_C)))
+    std_CCL.append(np.sqrt(p_CCL * (1 - p_CCL)))
+    std_mix.append(np.sqrt(p_CCL * (1 - p_mix)))
+    
+    if sum(x_C)==0:
+        rate_C.append(0)        
+    else:
+       rate_C.append(sum(x_C)/len(x_C))  
+    if sum(x_CCL)==0:
+        rate_CCL.append(0) 
+    else:
+        rate_CCL.append(sum(x_CCL)/len(x_CCL))
+    if sum(x_mix)==0:
+        rate_mix.append(0) 
+    else:
+        rate_mix.append(sum(x_mix)/len(x_mix))
+    x_C.clear()
+    x_CCL.clear()
+    x_mix.clear()
+
+# Qualitative evaluation
+plt.figure()
+# plt.plot(k, rate_CCL, 'ro', label="CCL")
+plt.errorbar(k, rate_CCL, yerr=std_CCL, fmt='o', color="blue", 
+             ecolor="black", elinewidth=1, capsize=5, label="CCL")
+
+plt.errorbar(k, rate_C, yerr=std_C, fmt='o', color="red", 
+             ecolor="black", elinewidth=1, capsize=5, label="C")
+# plt.plot(k, rate_C, 'bo', label="C")
+plt.xlabel("K")
+plt.ylabel("Failure Rate")
+plt.title("Qualitative evaluation")
+plt.grid()
+plt.legend()
+plt.savefig(f"plots/qualitative_K{m}_rep{len(seed)}_th{args.threshold}.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+
+# Qualitative evaluation with mix algoritm
+plt.figure()
+# plt.plot(k, rate_CCL, 'ro', label="CCL")
+plt.errorbar(k, rate_CCL, yerr=std_CCL, fmt='o', color="blue", 
+             ecolor="black", elinewidth=1, capsize=5, label="CCL (separed)")
+
+plt.errorbar(k, rate_C, yerr=std_C, fmt='o', color="red", 
+             ecolor="black", elinewidth=1, capsize=5, label="C")
+
+plt.errorbar(k, rate_mix, yerr=std_mix, fmt='o', color="orange", 
+             ecolor="black", elinewidth=1, capsize=5, label="CCL (mixed)")
+
+# plt.plot(k, rate_C, 'bo', label="C")
+plt.xlabel("K")
+plt.ylabel("Failure Rate")
+plt.title("Qualitative evaluation")
+plt.grid()
+plt.legend()
+plt.savefig(f"plots/qualitative_K{m}_rep{len(seed)}_th{args.threshold}_mix.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+
+
+
